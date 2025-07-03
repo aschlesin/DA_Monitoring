e7d87acb6f8b159060da5bc11c191491b91951f1
aschlesin <schlesin.angela@gmail.com>
2025-07-03 12:27:52 -0700

    add BarkleyNode

---
+++ dashboards_convert/BarkleyNode_devices_gen.py
@@ -0,0 +1,8 @@
+from util import template_gen
+
+location_code = "BACND"
+location_name = "Barkley Node"
+html_filename = "BarkleyNode_Devices"
+json_filename = "BarkleyNode_Devices"
+
+template_gen(location_code, location_name, html_filename, json_filename)
