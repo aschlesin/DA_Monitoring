72f2ce7bdcc0abc0c0e72a9174cdef0bb2242138
aschlesin <schlesin.angela@gmail.com>
2025-07-03 09:26:28 -0700

    barkley devices

--- dashboards_convert/Barkley_devices_gen.py
+++ dashboards_convert/Barkley_devices_gen.py
@@ -2,7 +2,7 @@ from util import template_gen
 
 location_code = "BAC"
 location_name = "Barkley"
-html_filename = "Barkley_devices"
-json_filename = "Barkley_devices"
+html_filename = "Barkley_Devices"
+json_filename = "Barkley_Devices"
 
 template_gen(location_code, location_name, html_filename, json_filename)
