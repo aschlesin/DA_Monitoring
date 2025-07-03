cd310e4909d52464a71e2cebe6e10afdff37f5ec
aschlesin <schlesin.angela@gmail.com>
2025-07-03 12:41:38 -0700

    add BarkleyUpperSlope

---
+++ dashboards_convert/BarkleyUpperSlope_devices_gen.py
@@ -0,0 +1,8 @@
+from util import template_gen
+
+location_code = "NCBC"
+location_name = "Barkley Upper Slope"
+html_filename = "BarkleyUpperSlope_Devices"
+json_filename = "BarkleyUpperSlope_Devices"
+
+template_gen(location_code, location_name, html_filename, json_filename)
